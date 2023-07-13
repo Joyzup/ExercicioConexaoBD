074ab828e8b621c0c88f11ff6f167585543144ab
carloslaczynskizup <carlos.laczynski@zup.com.br>
2023-07-13 09:45:19 -0300

    UPLOAD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import model.Usuario;
 import service.ExecutarService;
 
 public class Main {
@@ -6,118 +7,19 @@ public class Main {
 
         ExecutarService executarService = new ExecutarService();
         executarService.consultarBD();
+
         executarService.inserirDados();
         executarService.alterarDados();
 
         executarService.inserirDados();
         executarService.consultarNome();
 
+//        Usuario usuario = new Usuario();
+//        usuario.getNome();
+
         executarService.consultarBD();
 
         executarService.deletarDados();
 
-
-
-
-
-
-        //Conexão com o banco de dados
-
-//        try {
-//            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testebancodedados",
-//                    "postgres", "Postgres@1982");
-//
-//            if (connection != null) {
-//                System.out.println("|--------------------------------------------------|");
-//                System.out.println("| Conexão ao Banco de Dados realizada com sucesso! |");
-//                System.out.println("|--------------------------------------------------|");
-//                Statement statement = connection.createStatement();
-//
-//
-//
-//                consultarBD(statement);
-//                inserirDados(statement);
-//                alterarDados(statement);
-//                inserirDados(statement);
-//                consultarNome(statement);
-//
-//                consultarBD(statement);
-//
-////               deletarDados(statement);
-//
-//            } else {
-//                System.out.println("|------------------------------------------|");
-//                System.out.println("| Conexão ao banco de dados não realizada! |");
-//                System.out.println("|------------------------------------------|");
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
     }
-
-    //Manipular o banco de dados e método para consultar o banco de dados
-//    static void consultarBD(Statement statement) {
-//        //consulta por completo o BD
-//        String sql = "SELECT * FROM usuarios";
-//        try {
-//            ResultSet rs = statement.executeQuery(sql);
-//            while (rs.next()) {
-//                System.out.println("ID: " + rs.getInt("id_usuario") + " - Nome: "
-//                        + rs.getString("nome") + " - Idade: " + rs.getInt("idade") + " anos.");
-//            }
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
-//    }
-    //método para inserir dados
-//    static void inserirDados(Statement statement){
-//        String sql = "INSERT INTO usuarios (nome, idade) VALUES " + "('Gabriel Lopes', 30), " + "('João Pedro', 25)";
-//        try {
-//            statement.executeUpdate(sql);
-//            System.out.println("Dados inseridos com sucesso!");
-//            System.out.println();
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        }
-
-        //método para alterar dados da tabela do BD
-//    static void alterarDados(Statement statement){
-//        String sql = "UPDATE usuarios SET nome = 'João Henrique' WHERE id_usuario = 1";
-//        try {
-//            statement.executeUpdate(sql);
-//            System.out.println("Dados alterados com sucesso!");
-//            System.out.println();
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    //método para deletar dados da tabela do BD
-//    static void deletarDados(Statement statement){
-//        String sql = "DELETE FROM usuarios WHERE id_usuario BETWEEN 8 AND 30";
-//        try {
-//            statement.executeUpdate(sql);
-//            System.out.println("Dados ID entre 8 e 30 - deletados com sucesso!");
-//            System.out.println();
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//método para consultar campo nome da tabela do BD
-//    static void consultarNome(Statement statement){
-//        String sql = "SELECT nome FROM usuarios";
-//        try {
-//            ResultSet rs = statement.executeQuery(sql);
-//            while (rs.next()) {
-//                System.out.println("Nome: " + rs.getString("nome"));
-//                System.out.println();
-//            }
-//        }catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    }
+}
--- src/main/java/service/ExecutarService.java
+++ src/main/java/service/ExecutarService.java
@@ -1,75 +1,79 @@
 package service;
 
 import connection.Conexao;
+import model.Usuario;
+
 import java.sql.*;
 
 public class ExecutarService {
     static Connection connection = Conexao.getConnection();
     static Statement statement;
 
-   static {
-       try {
-           statement = connection.createStatement();
-       } catch (SQLException e) {
-           throw new RuntimeException(e);
-       }
-   }
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-    public void consultarBD(){
-        String sql = "SELECT nome FROM usuarios";
+    public void consultarBD() {
+        String sql = "SELECT * FROM usuarios";
         try {
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {
-                System.out.println("ID: " + rs.getInt("id_usuario") + " - Nome: " + rs.getString("nome") + " - Idade: " + rs.getInt("idade") + " anos.");
+                System.out.println("ID: " + rs.getInt("id_usuario") + " - Nome: "
+                        + rs.getString("nome") + " - Idade: " + rs.getInt("idade") + " anos.");
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
 
-    public void inserirDados(){
-        String sql = "INSERT INTO usuarios (nome, idade) VALUES " + "('Gabriel Lopes', 30), " + "('João Pedro', 25)";
+    public void inserirDados() {
+        String sql = "INSERT INTO usuarios (nome, idade) VALUES ('Gabriel Lopes', 30), ('João Pedro', 25)";
         try {
             statement.executeUpdate(sql);
             System.out.println("Dados inseridos com sucesso!");
             System.out.println();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        }
+    }
 
-    public void alterarDados(){
-        String sql = "UPDATE usuarios SET nome = 'João Henrique' WHERE id_usuario = 1";
+    public void alterarDados() {
+        String sql = "UPDATE usuarios SET nome = 'João Henrique' WHERE id_usuario = 3";
         try {
             statement.executeUpdate(sql);
             System.out.println("Dados alterados com sucesso!");
             System.out.println();
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    public void deletarDados(){
-        String sql = "DELETE FROM usuarios WHERE id_usuario BETWEEN 31 AND 100";
-        try {
-            statement.executeUpdate(sql);
-            System.out.println("Dados ID entre 8 e 30 - deletados com sucesso!");
-            System.out.println();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-//    método para consultar campo nome da tabela do BD
-    public void consultarNome(){
+    public void deletarDados() {
+        String sql = "DELETE FROM usuarios WHERE id_usuario BETWEEN 8 AND 15";
+        try {
+            statement.executeUpdate(sql);
+            System.out.println("Dados ID deletados com sucesso!");
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    método para consultar campo nome da tabela do BD
+    public void consultarNome() {
         String sql = "SELECT nome FROM usuarios";
         try {
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {
                 System.out.println("Nome: " + rs.getString("nome"));
-                System.out.println();
+
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
