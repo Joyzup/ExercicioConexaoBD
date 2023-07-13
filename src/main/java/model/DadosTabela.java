de5e3427b23d657c0ff163248f6e347f1ebae5c1
Kael-g <mikael.gualdi@zup.com.br>
2023-07-12 23:54:30 -0300

    Modificando formatação de exibição de dados

--- src/main/java/model/DadosTabela.java
+++ src/main/java/model/DadosTabela.java
@@ -7,10 +7,18 @@ import java.sql.Statement;
 public class DadosTabela {
     public void mostrarTodosOsDados(Statement statement){
         String sql = "select * from pessoas";
+        boolean primeiroLaco = true;
         try {
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
-                System.out.println("id: " + resultSet.getInt("id_pessoa") + " Nome: " + resultSet.getString("nome"));
+                if (primeiroLaco){
+                    System.out.printf("%-10s %s\n", "ID", "NOME");
+                    primeiroLaco = false;
+                }
+                System.out.printf("%-10s %s\n", resultSet.getInt("id_pessoa"), resultSet.getString("nome"));
+            }
+            if (primeiroLaco){
+                System.out.println("Não há usuários registrados");
             }
         } catch (SQLException e){
             e.printStackTrace();
@@ -19,10 +27,18 @@ public class DadosTabela {
 
     public void mostrarNomes(Statement statement){
         String sql = "select nome from pessoas";
+        boolean primeiroLaco = true;
         try {
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
+                if (primeiroLaco){
+                    System.out.println("NOME");
+                    primeiroLaco = false;
+                }
                 System.out.println(resultSet.getString("nome"));
+            }
+            if (primeiroLaco){
+                System.out.println("Não há usuários registrados");
             }
         } catch (SQLException e){
             e.printStackTrace();
