8407213de5bd6c5669b5089055bd3a08eadfa1e3
WellZup <wellington.oliveira@zup.com.br>
2023-07-12 21:48:49 -0300

    :recycle:Melhorias

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,77 @@
+import model.Usuario;
+import service.ServiceUser;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.Executor;
+
 public class Main {
     //Essa classe vai ser responsável pela execução da aplicação
     public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+        Usuario usuario = new Usuario();
+        String menuOpcao, nome , alterarNome;
+        Integer id, idExcluir;
+
+        ServiceUser serviceUser = new ServiceUser();
+
+        System.out.println("===============================");
+        System.out.println("=========== ZUPFood ===========");
+
+
+        boolean continuar = true;
+
+        do {
+            menuOpcao = exibirMenu();
+            switch (menuOpcao) {
+                case "1" :
+                    serviceUser.consultaDados();
+                    break;
+                case "2" :
+                    System.out.println("Para inserir usuário informe o nome: ");
+                    nome = entrada.next();
+                    break;
+                case "3" :
+                    System.out.println("Para alterar o nome informe o ID: ");
+                    id = entrada.nextInt();
+                    System.out.println("Novo nome: ");
+                    alterarNome = entrada.next();
+                    usuario.setNome(alterarNome);
+                case "4" :
+                    System.out.println("Para excluir usuário informe o ID: ");
+                    idExcluir = entrada.nextInt();
+
+                    serviceUser.excluirDados(idExcluir);
+                    break;
+                case "5" :
+                    serviceUser.consultarNome();
+                    break;
+                case "6" :
+                    continuar = false;
+                    System.out.println("Sair do programa.");
+                    break;
+
+            }
+        } while (continuar);
 
 
 
+    }
+
+    private static String exibirMenu() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("===============================");
+        System.out.println("===Escolha a opção desejada====");
+        System.out.println("=== #1 - Consultar usuário ====");
+        System.out.println("=== #2 - Cadastrar usuário ====");
+        System.out.println("=== #3 - Atualizar usuário ====");
+        System.out.println("=== #4 - Deletar usuário ======");
+        System.out.println("=== #5 - Consultar nomes ======");
+        System.out.println("=== #6 - Sair do programa =====");
+        System.out.println("===============================");
+        return entrada.nextLine();
     }
 }
--- src/main/java/service/ServiceUser.java
+++ src/main/java/service/ServiceUser.java
@@ -1,2 +1,85 @@
-package service;public class serviceUser {
+package service;
+
+import connection.Conexao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ServiceUser {
+
+    String sql;
+    ResultSet resultSet;
+
+
+    static Connection connection = Conexao.getConnection();
+
+    static Statement statement;
+
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw  new RuntimeException(e);
+        }
+    }
+
+    public void consultaDados () {
+         sql = "select * from usuarios";
+        try {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                System.out.println("id: "+ resultSet.getInt("id_usuario") + " nome: " +
+                        resultSet.getString("nome"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void incluirDados (String nome) {
+        sql = String.format("insert into usuario(nome) values ('%s')", nome);
+        try {
+            statement.executeUpdate(sql);
+            System.out.println("Usuário adicionado.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void alterarDados (String nome, Integer idUsuario) {
+        sql = String.format("update usuarios set nome= '%s' where id_usuario= %d" , nome, idUsuario);
+        try {
+            statement.executeUpdate(sql);
+            System.out.println("Dados do usuário alterados.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void excluirDados (Integer idUsuario) {
+        sql = String.format("delete from usuarios where id_usuarios=%d" , idUsuario);
+        try {
+            statement.executeUpdate(sql);
+            System.out.println("Usuário excluído.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void consultarNome () {
+        sql = "select nome from usuarios";
+        try {
+           resultSet = statement.executeQuery(sql);
+           while (resultSet.next()) {
+               System.out.println("nome: " + resultSet.getString("nome"));
+           }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
